1fc549cda686a9c6179615aedd85a258097543f6
CreationsCaplette <david.caplette@gmail.com>
2026-07-19 18:25:48 +0200

    Added mapping to score and results

--- geo-quiz/src/scenes/GameOver.jsx
+++ geo-quiz/src/scenes/GameOver.jsx
@@ -1,4 +1,5 @@
 import { useContext } from 'react';
+import GameContext from '../store/GameContext.jsx';
 import SceneContext from '../store/SceneContext.jsx';
 import { SCENES } from "../store/scenes.js";
 import faceBad from '../assets/face_bad.svg';
@@ -11,16 +12,29 @@ import PreviousButton from '../components/PreviousButton.jsx';
 export default function Menu() {
     const { goToScene } = useContext(SceneContext);
 
+    const {
+        score,
+    } = useContext(GameContext);
+
+    const resultByRange = [
+        { min: 800, src: faceExcellent, encouragement: "Wow! Excellent!" },
+        { min: 600, src: faceGood, encouragement: "Very well!" },
+        { min: 300, src: faceOkay, encouragement: "Keep on learning!" },
+        { min: 0, src: faceBad, encouragement: "Learn more!" },
+    ];
+
+    const result = resultByRange.find(item => score >= item.min);
+
     return (
-        <div className="screen game-over">
+        <div className="screen">
             <h2 className="game-over-title">Result</h2>
-            <p className="game-over-score">Score: 0</p>
+            <p className="game-over-score">Score: {score}</p>
             <p className="game-over-best-score">You best score: 0</p>
-            <p className="game-over-ecouragement">Learn more!</p>
+            <p className="game-over-ecouragement">{result.encouragement}</p>
             <img
-                src={faceBad}
+                src={result.src}
                 className="screen__image screen__image--small"
-                alt="Face Bad"
+                alt="Result face"
             />
             <PreviousButton onClick={() => goToScene(SCENES.menu)}>
                 Go Back
